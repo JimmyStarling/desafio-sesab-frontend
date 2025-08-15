6ce76b0366f5beb82a23d3446ed701d8b35c6428
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-15 19:13:56 -0300

    feat(views): add logout view

---
+++ src/views/LogoutView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="logout-view">
+        <h1>Você acabou de sair</h1>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+    setTimeout(() => {
+        router.replace({ name: 'LoginView' })
+    }, 1500) // 1.5 seconds delay
+})
+</script>
+
+<style scoped>
+.logout-view {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-family: sans-serif;
+}
+</style>
