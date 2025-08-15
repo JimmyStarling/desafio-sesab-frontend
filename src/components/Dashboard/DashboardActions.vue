df069ff815d8bf206bc06d9fdffd75a8ce2c5882
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-15 17:48:10 -0300

    feat(actions): add dashboard actions

---
+++ src/components/Dashboard/DashboardActions.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dashboard-actions">
+    <button @click="logout" class="logout-btn">Sair</button>
+    <button @click="$emit('novo')" class="novo-btn">Novo</button>
+    <button @click="$emit('filtrar')" class="filtrar-btn">Filtrar</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const auth = useAuthStore()
+
+function logout() {
+  auth.logout()
+  router.push({ name: 'LogoutView' })
+}
+</script>
