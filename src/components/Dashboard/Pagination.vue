4d16662ebbfab3867c7c081df3c2b93acb559235
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-08-15 17:51:00 -0300

    feat(components): add generic pagination to be used by users table

---
+++ src/components/Dashboard/Pagination.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pagination">
+    <button :disabled="page === 1" @click="$emit('changePage', page - 1)">Anterior</button>
+    <span>Página {{ page }} de {{ totalPages }}</span>
+    <button :disabled="page === totalPages" @click="$emit('changePage', page + 1)">Próxima</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  page: number
+  totalPages: number
+}>()
+</script>
